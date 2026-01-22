6c7c716bf69749de0aa805eed54c5499fd803a6d
Balastov <m0000nspe1111@gmail.com>
2026-01-22 13:07:44 +0300

    Добавил Импорты

---
+++ InFlameBody.py
@@ -0,0 +1,24 @@
+#  8537827750:AAH7IXmQNNNdX3RQOg4REm5BFnCMYtqjK5s
+
+import random
+from http.client import responses
+
+from datetime import datetime
+import telebot
+from bs4 import BeautifulSoup
+import requests
+import sqlite3
+import threading
+import time
+
+# from telegram._utils import markup
+
+# from QuestionsSender import QuestionsSender
+# from MemeSender import MemeSender
+# import os
+
+# import random
+
+# from telegram._utils import markup
+
+bot = telebot.TeleBot('8537827750:AAH7IXmQNNNdX3RQOg4REm5BFnCMYtqjK5s')
